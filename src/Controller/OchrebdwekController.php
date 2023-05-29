2ddda05a5ce1da71975ddfb5566b3b72a2182f3f
mohamed masmoudi <73141406+enigma522@users.noreply.github.com>
2023-05-29 15:55:27 +0100

    Update OchrebdwekController.php

--- src/Controller/OchrebdwekController.php
+++ src/Controller/OchrebdwekController.php
@@ -22,93 +22,91 @@ use Twilio\Rest\Client;
 class OchrebdwekController extends AbstractController
 {
 
-    public function __construct(private UsersRepository $usersRepository,private TodoEventsRepository $todoEventsRepository,private TodoRepository $todoRepository)
+    public function __construct(private UsersRepository $usersRepository, private TodoEventsRepository $todoEventsRepository, private TodoRepository $todoRepository)
     {
-        
-        
     }
     #[Route('/ochrobdwek', name: 'app_ochrob_dwek')]
-    public function index(Request $request,Security $security): Response
+    public function index(Request $request, Security $security): Response
     {
-        
-        
-        $event=new Todo();
-        $form=$this->createForm(TodoType::class,$event
+
+
+        $event = new Todo();
+        $form = $this->createForm(
+            TodoType::class,
+            $event
         );
         $form->handleRequest($request);
 
-        
-        if($form->isSubmitted())
-        {$phoneNum=$this->usersRepository->findOneBy(['email' =>$event->getPatientemail()])->getPhoneNumber();
-            
-            $sid = "AC06e5605c4af770c4e04ab90c2bb273c5"; // Your Account SID from https://console.twilio.com
-    $token = "ca2b1a4f1ff1a1095a9ab8704c56bcad"; // Your Auth Token from https://console.twilio.com
-    $client = new Client($sid, $token);
-    $receiver = "+" .$phoneNum. "";
-    $dateDebut = $event->getDateDebut()->format('Y-m-d');
-    // Use the Client to make requests to the Twilio REST API
-    $message = $client->messages->create(
-        // The number you'd like to send the message to
-        $receiver,
-        [
-            // A Twilio phone number you purchased at https://console.twilio.com
-            'from' => '+15077283386',
-            // The body of the text message you'd like to send
-            // 'body' => "Hi " . $name . " your otp code is: " . $otp
-            'body' => "Dr ".$security->getUser()->getFirstName()." ".
-            $security->getUser()->getLastName()." vous a ajouté un nouveau evènement qui commence le  ".
-            $dateDebut.".\n"."MEDIRAPP"
-        ]
-    );
-    if (isset($message)) {
-    } else {
-        echo "Code failed";
-    }
 
-            $this->todoRepository->save($event,true);
-            $medecin = $this->usersRepository->findOneBy(['email' =>$security->getUser()->getUserIdentifier()]);
+        if ($form->isSubmitted()) {
+            $phoneNum = $this->usersRepository->findOneBy(['email' => $event->getPatientemail()])->getPhoneNumber();
 
-        $email = $event->getPatientEmail();
-        $patient = $this->usersRepository->findOneBy(['email' => $email]);
-        
-                $startDate = $event->getDateDebut();
-                $endDate = $event->getDateFin();
-                $endDate->modify('+1 day');
-                
-                // Loop through each day within the date range
-                $interval = new DateInterval('P1D');
-                $dateRange = new DatePeriod($startDate, $interval, $endDate);
-                foreach ($dateRange as $date) {
-                    // Create a new sub-object for the current day
-                    $subObj = [
-                        'name' => $event->getName(),
-                        'date' => $date->format('Y-m-d'),
-                        'description' => $event->getDescription(),
-                        'type' => 'event',
-                        'patient_email' => $event->getPatientEmail(),
-                    ];
-                    $todo=new TodoEvents();
-                    $date = new DateTime($subObj['date']);
-                    $todo->setName($subObj['name']);
-                    $todo->setDate($date);
-                    $todo->setDescription($subObj['description']);
-                    $todo->setType($subObj['type']);
-                   // $todo->setPatientEmail($subObj['patient_email']);
-                    $todo->setPatient($patient);
-                    $this->todoEventsRepository->save($todo,true);
-                    
-                }
-                
-            $this->addFlash('success',$event->getName()."a été ajouté avec succès");
-        }
-        else{
-            $this->addFlash('error',"erreur");
+            $sid = 0; // Your Account SID from https://console.twilio.com
+            $token = 0; // Your Auth Token from https://console.twilio.com
+            $client = new Client($sid, $token);
+            $receiver = "+" . $phoneNum . "";
+            $dateDebut = $event->getDateDebut()->format('Y-m-d');
+            // Use the Client to make requests to the Twilio REST API
+            $message = $client->messages->create(
+                // The number you'd like to send the message to
+                $receiver,
+                [
+                    // A Twilio phone number you purchased at https://console.twilio.com
+                    'from' => '+15077283386',
+                    // The body of the text message you'd like to send
+                    // 'body' => "Hi " . $name . " your otp code is: " . $otp
+                    'body' => "Dr " . $security->getUser()->getFirstName() . " " .
+                        $security->getUser()->getLastName() . " vous a ajouté un nouveau evènement qui commence le  " .
+                        $dateDebut . ".\n" . "MEDIRAPP"
+                ]
+            );
+            if (isset($message)) {
+            } else {
+                echo "Code failed";
+            }
+
+            $this->todoRepository->save($event, true);
+            $medecin = $this->usersRepository->findOneBy(['email' => $security->getUser()->getUserIdentifier()]);
+
+            $email = $event->getPatientEmail();
+            $patient = $this->usersRepository->findOneBy(['email' => $email]);
+
+            $startDate = $event->getDateDebut();
+            $endDate = $event->getDateFin();
+            $endDate->modify('+1 day');
+
+            // Loop through each day within the date range
+            $interval = new DateInterval('P1D');
+            $dateRange = new DatePeriod($startDate, $interval, $endDate);
+            foreach ($dateRange as $date) {
+                // Create a new sub-object for the current day
+                $subObj = [
+                    'name' => $event->getName(),
+                    'date' => $date->format('Y-m-d'),
+                    'description' => $event->getDescription(),
+                    'type' => 'event',
+                    'patient_email' => $event->getPatientEmail(),
+                ];
+                $todo = new TodoEvents();
+                $date = new DateTime($subObj['date']);
+                $todo->setName($subObj['name']);
+                $todo->setDate($date);
+                $todo->setDescription($subObj['description']);
+                $todo->setType($subObj['type']);
+                // $todo->setPatientEmail($subObj['patient_email']);
+                $todo->setPatient($patient);
+                $this->todoEventsRepository->save($todo, true);
+            }
+
+            $this->addFlash('success', $event->getName() . "a été ajouté avec succès");
+        } else {
+            $this->addFlash('error', "erreur");
         }
         return $this->render('ochrebdwek/index.html.twig', [
             'controller_name' => 'OchrobDwekController',
-            'form'=>$form->createView(),
-            'login'=>'logout',
-            'page'=>'Ochrob Dwek'
+            'form' => $form->createView(),
+            'login' => 'logout',
+            'page' => 'Ochrob Dwek'
         ]);
     }
 }
